fd0864a5abae83b7f1144a8df8c9f4e34ef0022e
Dmitriy Domrachev <dimd1288@gmail.com>
2023-02-25 16:30:16 +0300

    main controllers added

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
-// Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`)
-})
+app.use((req, res, next) => {
+  req.user = {
+    _id: '63f8b14b28e55dc080fb9434'
+  };
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+
+
+app.listen(PORT);
